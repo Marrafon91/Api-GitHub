cb17821630aa306847af28c3162dafbce6c7fe25
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-04 19:55:46 -0300

    implements basic CSS

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return <h1 className="text-red-500 font-bold h-screen flex justify-center items-center text-3xl">Home</h1>;
 }
--- src/pages/Repositorio/index.tsx
+++ src/pages/Repositorio/index.tsx
@@ -1,5 +1,7 @@
 export default function Repositorio() {
   return (
-    <h1>Repositorio</h1>
-  )
+    <h1 className="flex h-screen items-center justify-center text-3xl font-bold text-red-500">
+      Repositorio
+    </h1>
+  );
 }
