176fd8dc07dcf8f08ed9d3f5fb0d6225621a38c4
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-04 19:17:58 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-10">
+      <h1 className="flex items-center justify-center rounded-lg bg-blue-500 p-8 text-4xl font-bold text-white shadow-lg">
+        Olá Mundo
+      </h1>
+    </div>
+  );
+}
